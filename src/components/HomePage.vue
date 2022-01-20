461e99f2cb4db9eac31b2df7ce5bcee713f327dc
Kinga <kinga.sabat@gmail.com>
2022-01-20 17:01:38 +0100

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  setup() {
+
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../variables';
+
+</style>
